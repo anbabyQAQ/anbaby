a2ce609c631ebcd992c8333925a60d2e31b4fcfd
shiyan <tianyulong@163.com>
2016-08-17 18:02:57 +0800

    增加血糖界面

---
+++ YiJianBluetooth/classes/Modules/index/Controller/DetailController/血糖/BSViewController.h
@@ -0,0 +1,13 @@
+//
+//  BSViewController.h
+//  YiJianBluetooth
+//
+//  Created by 孙程雷Mac on 16/8/17.
+//  Copyright © 2016年 LEI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BSViewController : UIViewController
+
+@end
---
+++ YiJianBluetooth/classes/Modules/index/Controller/DetailController/血糖/BSViewController.m
@@ -0,0 +1,37 @@
+//
+//  BSViewController.m
+//  YiJianBluetooth
+//
+//  Created by 孙程雷Mac on 16/8/17.
+//  Copyright © 2016年 LEI. All rights reserved.
+//
+
+#import "BSViewController.h"
+
+@interface BSViewController ()
+
+@end
+
+@implementation BSViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
